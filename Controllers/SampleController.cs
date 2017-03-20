be885b56cca1c241f1ce1912a01e3b541b140bd3
Jesú Márquez <jesumarquez@gmail.com>
2017-03-20 13:26:06 -0300

    Logger

--- Controllers/SampleController.cs
+++ Controllers/SampleController.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace aspnetcoremvc.Controllers
@@ -8,13 +9,16 @@ namespace aspnetcoremvc.Controllers
     public class SampleController: Controller
     {
         private readonly GeneralOptions _options;
-        public SampleController(IOptions<GeneralOptions> options)
+        private readonly ILogger _logger;
+        public SampleController(IOptions<GeneralOptions> options, ILogger<SampleController> logger)
         {
             _options = options.Value;
+            _logger = logger;
         }
         public ActionResult Index()
         {
             string option1 = _options.Option1;
+            _logger.LogWarning(option1);
             return View("Index", option1);
         }
     }
